f55efa6070e80db021c4be5effa588e93ae46ab4
ARIJIT GHOSH <168671355+arijitghosh8@users.noreply.github.com>
2024-08-08 13:24:44 +0530

    Delete 9. Creating a Spring Boot Application/LibraryManagement/LibraryManagement directory

--- 9. Creating a Spring Boot Application/LibraryManagement/LibraryManagement/src/main/java/com/library/api/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.library.api.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
